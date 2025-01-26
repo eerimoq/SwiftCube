a662ab5f5ca8963219fd7ac22cf34ccc88f16ecd
Erik Moqvist <erik.moqvist@gmail.com>
2025-01-26 18:17:03 +0100

    Refactoring.

--- Sources/SwiftCube/SwiftCube.swift
+++ Sources/SwiftCube/SwiftCube.swift
@@ -1,26 +1,30 @@
-import BinaryCodable
 import CoreImage
 import CoreImage.CIFilterBuiltins
-
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
 import Foundation
 
+public struct LutEntry {
+    let red: Float
+    let green: Float
+    let blue: Float
+}
+
+func parseNumber(_ value: Substring) throws -> Float {
+    guard let number = Float(value) else {
+        throw SwiftCubeError.invalidDataPoint
+    }
+    return number
+}
+
 /// SwiftCube representation of a 3D LUT
-public struct SC3DLut: CustomDebugStringConvertible, Codable {
-    public var title: String? = nil
-    public var type: LUTType! = nil
-    public var size: Int! = nil
-    public var data: [[Float]] = []
+public struct SC3DLut {
+    public var title: String?
+    public var type: LutType!
+    public var size: Int!
+    public var entries: [LutEntry] = []
 
     /// Initialize a LUT from a URL
     public init(contentsOf url: URL) throws {
         try self.init(fileData: Data(contentsOf: url))
-    }
-
-    /// Initialize a LUT from a Data object generated from the .rawDataRepresentation() method
-    public init(dataRepresentation: Data) throws {
-        self = try BinaryDecoder().decode(SC3DLut.self, from: dataRepresentation)
     }
 
     /// Initialize a LUT from a .cube file's data
@@ -30,7 +34,7 @@ public struct SC3DLut: CustomDebugStringConvertible, Codable {
             throw SwiftCubeError.couldNotDecodeData
         }
         for line in stringData.components(separatedBy: "\n") {
-            guard !line.isEmpty && line.first != "#" else {
+            guard !line.isEmpty, line.first != "#" else {
                 continue
             }
             let parts = line.split(separator: " ")
@@ -50,12 +54,12 @@ public struct SC3DLut: CustomDebugStringConvertible, Codable {
             case "DOMAIN_MAX":
                 throw SwiftCubeError.unsupportedKey
             default:
-                try data.append(parts.map {
-                    guard let double = Float($0) else {
-                        throw SwiftCubeError.invalidDataPoint
-                    }
-                    return double
-                })
+                guard parts.count == 3 else {
+                    throw SwiftCubeError.invalidDataPoint
+                }
+                try entries.append(LutEntry(red: parseNumber(parts[0]),
+                                            green: parseNumber(parts[1]),
+                                            blue: parseNumber(parts[2])))
             }
         }
         guard size != nil else {
@@ -66,18 +70,13 @@ public struct SC3DLut: CustomDebugStringConvertible, Codable {
         }
     }
 
-    public var debugDescription: String {
-        return "LUT \(title ?? "") \(type) \(size) \(data.count)"
-    }
-
     /// Generate a CIFilter in the current device colorspace
     public func ciFilter() throws -> CIFilter & CIColorCubeWithColorSpace {
         var data: [Float] = []
-        for line in self.data {
-            guard line.count == 3 else {
-                throw SwiftCubeError.invalidDataPoint
-            }
-            data += line
+        for entry in entries {
+            data.append(entry.red)
+            data.append(entry.green)
+            data.append(entry.blue)
             data.append(1.0)
         }
         let filter = CIFilter.colorCubeWithColorSpace()
@@ -86,16 +85,9 @@ public struct SC3DLut: CustomDebugStringConvertible, Codable {
         filter.colorSpace = CGColorSpaceCreateDeviceRGB()
         return filter
     }
-
-    /// Generate a binary Data representation of the LUT for storage
-    public func rawDataRepresentation() throws -> Data {
-        let encoder = BinaryEncoder()
-        let data = try encoder.encode(self)
-        return data
-    }
 }
 
-public enum LUTType: Codable {
+public enum LutType: Codable {
     case oneDimensional
     case threeDimensional
 }
